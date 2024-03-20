750aad67d1f6878cbe5dcd14a6da7e6433b47744
chomusuke9072 <chomusuke9072@gmail.com>
2024-03-20 17:57:56 +0900

    Add CommentList 파일 생성 , Update 기능 적용

---
+++ src/components/comment/CommentList.tsx
@@ -0,0 +1,76 @@
+"use client";
+import React, { useState, useEffect, useRef } from "react";
+import useComment from "../../hooks/useComment";
+import { Comment } from "@/types/comment-types";
+
+const CommentList: React.FC = () => {
+  const {
+    comments,
+    handleRemoveComment,
+    handleSetEditingComment,
+    handleEditComment,
+  } = useComment();
+  const [editingCommentId, setEditingCommentId] = useState<number | null>(null);
+  const [tempContent, setTempContent] = useState<string>("");
+  const editInputRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    if (editingCommentId !== null) {
+      editInputRef.current?.focus();
+    }
+  }, [editingCommentId]);
+
+  const handleEdit = (comment: Comment) => {
+    setEditingCommentId(comment.id);
+    setTempContent(comment.content);
+    handleSetEditingComment(comment);
+  };
+
+  const handleSave = (id: number) => {
+    const commentToEdit = comments.find((comment) => comment.id === id);
+    if (commentToEdit) {
+      const updatedComment = { ...commentToEdit, content: tempContent };
+      handleEditComment(updatedComment);
+      setEditingCommentId(null);
+    }
+  };
+
+  return (
+    <div>
+      {comments.length > 0 ? (
+        <ul>
+          {comments.map((comment) => (
+            <li key={comment.id}>
+              {editingCommentId === comment.id ? (
+                <>
+                  <input
+                    ref={editInputRef}
+                    type="text"
+                    value={tempContent}
+                    onChange={(e) => setTempContent(e.target.value)}
+                  />
+                  <button onClick={() => handleSave(comment.id)}>완료</button>
+                  <button onClick={() => handleRemoveComment(comment.id)}>
+                    삭제
+                  </button>
+                </>
+              ) : (
+                <>
+                  {comment.content}
+                  <button onClick={() => handleEdit(comment)}>수정</button>
+                  <button onClick={() => handleRemoveComment(comment.id)}>
+                    삭제
+                  </button>
+                </>
+              )}
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p>댓글이 없습니다.</p>
+      )}
+    </div>
+  );
+};
+
+export default CommentList;
