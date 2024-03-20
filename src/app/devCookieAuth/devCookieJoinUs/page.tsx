673f7167150dcec5bd220264c0937c736f555e05
SohyangO <shyang903@naver.com>
2024-03-20 12:21:40 +0900

    Add: Github 로그인 환경세팅

--- src/app/devCookieAuth/devCookieJoinUs/page.tsx
+++ src/app/devCookieAuth/devCookieJoinUs/page.tsx
@@ -1,7 +1,18 @@
 import React from "react";
 
 function page() {
-  return <div>devCookieJoinUs</div>;
+  return (
+    <div>
+      <form>
+        <label>닉네임</label>
+        <label>아이디</label>
+        <label>비밀번호</label>
+        <button>회원가입</button>
+      </form>
+      <button>구글</button>
+      <button>깃헙</button>
+    </div>
+  );
 }
 
 export default page;
