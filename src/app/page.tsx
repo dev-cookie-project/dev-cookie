cc91dc78be60ceb96072d7911aeb24c12075c11a
SohyangO <shyang903@naver.com>
2024-03-21 20:11:29 +0900

    Add: Nav 컴포넌트 생성

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ export default function Home() {
     <>
       <div>Next</div>
       <Link href="/devCookieAuth/devCookieJoinUs">회원가입 페이지</Link>
+      <Link href="/devCookieAuth/devCookieLogIn">로그인 페이지</Link>
     </>
   );
 }
