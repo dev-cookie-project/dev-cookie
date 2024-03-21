e75a05f54895a28bc960f1375bd24f3602acafd4
chomusuke9072 <chomusuke9072@gmail.com>
2024-03-21 09:09:17 +0900

    Update supabase 연동,댓글 추가 구현(새로고침 해야 UI반영됨)

--- src/components/comment/CommentForm.tsx
+++ src/components/comment/CommentForm.tsx
@@ -1,13 +1,11 @@
 "use client";
 import React, { useState } from "react";
-import useComment from "../../hooks/useComment";
-import { Comment } from "../../types/comment-types";
+import { supabase } from "../../utils/supabaseClient";
 
 const CommentForm: React.FC = () => {
   const [content, setContent] = useState<string>("");
-  const { handleAddComment } = useComment();
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
     if (!content.trim()) return;
@@ -15,15 +13,26 @@ const CommentForm: React.FC = () => {
     const userId = "1";
     const createdAt = new Date().toISOString();
 
-    const newComment: Comment = {
-      id: Date.now(),
-      content,
-      userId,
-      createdAt,
-    };
-    handleAddComment(newComment);
+    try {
+      const { data, error } = await supabase.from("comments").insert([
+        {
+          content,
+          userId,
+          createdAt,
+        },
+      ]);
 
-    setContent("");
+      if (error) {
+        throw error;
+      }
+
+      if (data) {
+        console.log("New comment added successfully:", data);
+        setContent("");
+      }
+    } catch (error) {
+      console.error("Error adding comment:", error);
+    }
   };
 
   return (
