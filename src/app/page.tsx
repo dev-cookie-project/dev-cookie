632b417381fe6f217f41b2da995e6e97e7d011cf
SohyangO <shyang903@naver.com>
2024-03-21 20:19:53 +0900

    Update: Topnav 바 만듦

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Link from "next/link";
+import TopNav from "./components/topNav";
 
 export default function Home() {
   return (
     <>
       <div>Next</div>
+      <TopNav></TopNav>
       <Link href="/devCookieAuth/devCookieJoinUs">회원가입 페이지</Link>
       <Link href="/devCookieAuth/devCookieLogIn">로그인 페이지</Link>
     </>
