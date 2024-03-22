52d7ed48d8e8f676505d16fa688c20dab1fc15c5
we-are-good <140328307+we-are-good@users.noreply.github.com>
2024-03-22 17:04:35 +0900

    "Fix: Project페이지의 grid 수정"

--- src/app/project/page.tsx
+++ src/app/project/page.tsx
@@ -6,7 +6,7 @@ function page() {
     <>
       <DevCookieProjectSearchForm />
       <div className="h-200 w-128 bg-orange-400 py-8 px-20">
-        <div className="grid grid-rows-3 grid-cols-3 gap-4">
+        <div className="grid grid-rows-2 grid-cols-3 gap-4">
           <div className="card w-96 bg-base-100 shadow-xl">
             <figure>
               <Image src="/cookies.png" alt="Burger" width="200" height="200" />
