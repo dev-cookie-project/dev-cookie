2fce4a1cb1b61b7e719e0f75b43da5a6dee46e33
ugyobppang <yeonguk0517@gmail.com>
2024-03-23 13:44:18 +0900

    feat: gathering type 및 커스텀 훅 추가

---
+++ src/hooks/useGathering.tsx
@@ -0,0 +1,42 @@
+import { Gathering } from '@/types/gathering-types';
+import { supabase } from '@/utils/supabaseGathering';
+import { useState } from 'react';
+
+const useGathering = () => {
+  const [gatherings, setGatherings] = useState<Gathering[]>();
+
+  // supabase 조회
+  const getGatherings = async () => {
+    const { data, error } = await supabase.from('gatherings').select('*');
+
+    if (data) {
+      setGatherings(data);
+    }
+
+    if (error) {
+      return alert('error 발생!');
+    }
+  };
+
+  // supabase 저장
+  const addGathering = async ({
+    gathering,
+    stack,
+    title,
+    content,
+    userId,
+  }: Gathering) => {
+    const { data, error } = await supabase
+      .from('gatherings')
+      .insert([{ gathering, stack, title, content, userId }])
+      .select();
+  };
+
+  // supabase 삭제
+
+  // supabase 수정
+
+  return { gatherings, getGatherings, addGathering };
+};
+
+export default useGathering;
