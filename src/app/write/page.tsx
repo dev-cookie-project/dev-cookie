9f2ef2f126f49cbec463f71b462e21874a313310
ugyobppang <yeonguk0517@gmail.com>
2024-03-23 16:25:08 +0900

    style: section 태그 width 수정

--- src/app/write/page.tsx
+++ src/app/write/page.tsx
@@ -7,7 +7,7 @@ function WritePage() {
       <section className="h-48 w-128 flex flex-row items-center justify-center gap-8 text-black text-2xl bg-orange-300">
         <h1>원하는 모임을 만들어 보세요!</h1>
       </section>
-      <section className="h-200 w-128 bg-orange-400 py-8 px-20">
+      <section className="h-screen w-full bg-orange-400 py-8 px-20">
         <GatheringForm />
       </section>
     </main>
